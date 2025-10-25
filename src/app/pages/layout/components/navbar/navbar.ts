ff0744f6238aaf8aad14e105a90f23d82b3fe982
saulemilioramirezcayoja08-cloud <saulemilioramirezcayoja08@gmail.com>
2025-10-25 04:30:11 -0400

    refactor navbar

--- src/app/pages/layout/components/navbar/navbar.ts
+++ src/app/pages/layout/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,32 @@ import {Router, RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  currentUrl: string = '';
+
+  private readonly routeMap = {
+    ventas: ['/quotation', '/order', '/sale', '/credit', '/payments', '/reservas'],
+    inventario: ['/inventory'],
+    compras: ['/purchase', '/suppliers'],
+    catalogo: ['/catalog'],
+    reportes: ['/report'],
+    clientes: ['/customers'],
+    documentos: ['/documents'],
+    administracion: ['/admin']
+  };
+
   constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentUrl = event.urlAfterRedirects;
+      });
+
+    this.currentUrl = this.router.url;
+  }
+
+  isModuleActive(module: keyof typeof this.routeMap): boolean {
+    const routes = this.routeMap[module];
+    return routes.some(route => this.currentUrl.startsWith(route));
   }
 
   logout() {
