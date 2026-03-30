af989b35a6fdda4b02c93d031784c933b4c44873
saulemilioramirezcayoja08-cloud <saulemilioramirezcayoja08@gmail.com>
2026-03-30 15:13:01 -0400

    5

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  api_url: 'https://boatable-dimple-dissolvingly.ngrok-free.dev/api'
+  api_url: 'https://2340-177-222-99-133.ngrok-free.app/api'
 };
